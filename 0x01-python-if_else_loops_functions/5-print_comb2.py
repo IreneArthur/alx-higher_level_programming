4b41f9fa4f11c181c16590a734343cb0798e655d
Arthur <shingiroirene@gmail.com>
2024-03-29 20:06:34 +0200

    99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,5 @@
 """" print numbers from 0 to 99 """
 
 for numbers in range(100):
-    print("{:2d}".format(numbers), end="")
+    print("{:02d}".format(numbers), end="")
     print(end=", "if numbers != 99 else "\n")
