200ea235b842934f65a4f5e72e3dbcf440dbcca7
Arthur <shingiroirene@gmail.com>
2024-03-29 19:34:57 +0200

    print alphabet without e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,5 @@
 """Print the alphabet in lowercase, not followed by a new line."""
 
 for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+    if letter != 101 | letter != 113:
+        print("{}".format(chr(letter)), end="")
