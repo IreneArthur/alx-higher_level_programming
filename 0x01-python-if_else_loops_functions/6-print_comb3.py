3b9019c9a8541087ad8adbd15fa42a6254c68b22
Arthur <shingiroirene@gmail.com>
2024-03-30 10:18:28 +0200

    2 pair combos

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """" print combo of 2 digits  """
 
-for i in range (10):
-    for j in range (i, 10):
+for i in range(10):
+    for j in range(i, 10):
         if i != j:
             print("{}{}".format(i, j), end="")
             print(end=", "if i != 8 or j != 9 else "\n")
