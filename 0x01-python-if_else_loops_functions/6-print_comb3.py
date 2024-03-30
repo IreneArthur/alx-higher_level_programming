dbf76eb6902f8357d5b0554ff7fe4bdb4b98e834
Arthur <shingiroirene@gmail.com>
2024-03-30 09:00:55 +0200

    possible 2 digits combos

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,6 @@
 
 for i in range (10):
     for j in range (i, 10):
-        print("{}{}".format(i, j), end="")
-        print(end=", "if i != 9 or j != 9 else "\n")
+        if i != j:
+            print("{}{}".format(i, j), end="")
+            print(end=", "if i != 8 or j != 9 else "\n")
