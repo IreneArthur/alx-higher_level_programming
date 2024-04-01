ad4dab4b9cae0ea1c10ab6ffcaaf63794ed64453
Arthur <shingiroirene@gmail.com>
2024-04-01 20:23:01 +0200

    max interger

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    for i in range(len(my_list)):
+        for j in range(len(my_list) - 1):
+            if my_list[j] > my_list[j + 1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j + 1]
+                my_list[j + 1] = temp
+    return (my_list[len(my_list) - 1])
